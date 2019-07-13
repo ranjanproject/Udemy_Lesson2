4c4190ceeca9398061b7ded50baab72dd1752d33
pratik <pratik.ranjan@testbook.com>
2019-07-14 02:18:26 +0530

    adding binding object

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.aboutme
 
 import android.content.Context
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,31 +9,44 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
-        val myButton = findViewById<Button>(R.id.button)
-
-        myButton.setOnClickListener {
+//        val myButton = findViewById<Button>(R.id.button)
+//
+//        myButton.setOnClickListener {
+//            addText(it)
+//        }
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.button.setOnClickListener {
             addText(it)
         }
 
     }
 
     fun addText(view: View){
-      val myText = findViewById<EditText>(R.id.myText)
-      val show = findViewById<TextView>(R.id.show)
+//      val myText = findViewById<EditText>(R.id.myText)
+//      val show = findViewById<TextView>(R.id.show)
 
-        show.text = myText.text
+//        show.text = myText.text
 //        myText.visibility = View.GONE
 //        view.visibility = View.GONE
-        show.visibility = View.VISIBLE
+//        show.visibility = View.VISIBLE
 
+        binding.apply {
+            invalidateAll()
+            show.text = myText.text
+            show.visibility = View.VISIBLE
+        }
         //hide keyboard from UI
 
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
